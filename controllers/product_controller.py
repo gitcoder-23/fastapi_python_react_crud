bf8d5f6ca34c26220be08b39ea9226a8c4054e3e
gitcoder-23 <prabhatece21@gmail.com>
2025-08-17 12:26:03 +0530

    product api done

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from models import (Product, product_pydantic, product_pydanticIn)
+from models import (Product, product_pydantic, product_pydanticIn, Supplier)
 
 router = APIRouter(
     prefix="/products",
@@ -33,17 +33,27 @@ async def update_product(product_id: int, update_info: product_pydanticIn): # ty
     product.quantity_in_stock = update_info['quantity_in_stock']
     product.quantity_sold = update_info['quantity_sold']
     product.unit_price = update_info['unit_price']
-    product.revenue = update_info['revenue']
+    product.revenue += update_info['quantity_sold'] * update_info['unit_price']
 
     await product.save()
 
-    # response = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
     response = await product_pydantic.from_tortoise_orm(product)
     return {'status': 'success', 'data': response}
 
 
 @router.delete("/{product_id}")
 async def delete_product(product_id: int):
-    # await Supplier.filter(id=supplier_id).delete()
-    await Product.get(id=product_id).delete()
+    await Product.filter(id=product_id).delete()
     return {'status': 'success', 'message': 'Product deleted'}
+
+
+# Product by supplier id
+@router.post("/product/{supplier_id}")
+async def add_products_by_supplier(supplier_id: int, product_details: product_pydanticIn ): # type: ignore
+    supplier = await Supplier.get(id=supplier_id)
+    product_details = product_details.dict(exclude_unset=True)
+    product_details['revenue'] += product_details['quantity_sold'] * product_details['unit_price']
+    product_obj = await Product.create(**product_details, supplied_by=supplier)
+    response = await product_pydantic.from_tortoise_orm(product_obj)
+    return {'status': 'success', 'data': response}
+
